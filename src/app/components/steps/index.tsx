a0b98c069d472b30779c56f81f18c4af66c152c2
Ashish Baravaliya <vier.institute35@gmail.com>
2023-07-07 12:18:20 -0400

    feat: updated successful pages

--- src/app/components/steps/index.tsx
+++ src/app/components/steps/index.tsx
@@ -1,10 +1,10 @@
 import styled from 'styled-components';
 
-const StepsContainer = styled.div((props) => ({
+const StepsContainer = styled.div<{withLabel?:boolean}>((props) => ({
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'space-between',
-  backgroundColor: props.theme.colors.background.elevation0_5,
+  backgroundColor: props.withLabel ? props.theme.colors.background.elevation0_5: "transparent",
   height: 40,
   borderRadius: props.theme.spacing(4),
   paddingLeft: props.theme.spacing(8),
@@ -14,7 +14,7 @@ const StepsContainer = styled.div((props) => ({
 const StepsLine = styled.div<StepLineProps>((props) => ({
   width: props.steps * 20,
   borderRadius: 2,
-  background: `linear-gradient(to right, ${props.theme.colors.action.classic} ${props.fill}%, ${props.theme.colors.background.elevation0} ${props.fill}%)`,
+  background: `linear-gradient(to right, ${props.theme.colors.action.classic} ${props.fill}%, ${ props.withLabel ? props.theme.colors.background.elevation0 : props.theme.colors.background.elevation2} ${props.fill}%)`,
   display: 'flex',
   height: 4,
 }));
@@ -36,14 +36,15 @@ interface StepLineProps {
   active: number;
   steps: number;
   fill: number;
+  withLabel?: boolean;
 }
 
 export default function Steps(props: StepsProps): JSX.Element {
   const { data, activeIndex, withLabel } = props;
   return (
-    <StepsContainer>
+    <StepsContainer withLabel={withLabel}>
       {withLabel && <Label>{`Step ${activeIndex + 1}`}</Label>}
-      {data.length ? <StepsLine active={activeIndex + 1} steps={data.length} fill={(activeIndex + 1 ) * 100 / data.length }/> : null}
+      {data.length ? <StepsLine active={activeIndex + 1} steps={data.length} fill={(activeIndex + 1 ) * 100 / data.length } withLabel={withLabel}/> : null}
     </StepsContainer>
   );
 }
