340c0cef1d41597697ad363f6343eebe5d127125
Ashish Baravaliya <vier.institute35@gmail.com>
2023-07-07 12:18:20 -0400

    feat: updated successful pages

--- src/app/screens/createWalletSuccess/index.tsx
+++ src/app/screens/createWalletSuccess/index.tsx
@@ -1,7 +1,11 @@
 import Completed from '@assets/img/createWalletSuccess/completed.png';
+import Followup from '@assets/img/createWalletSuccess/completedFollowup.png';
 import Extension from '@assets/img/createWalletSuccess/extension.svg';
 import Logo from '@assets/img/createWalletSuccess/logo.svg';
 import Pin from '@assets/img/createWalletSuccess/pin.svg';
+import ActionButton from '@components/button';
+import Steps from '@components/steps';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
@@ -44,7 +48,7 @@ const Image = styled.img((props) => ({
 const ContentContainer = styled.div((props) => ({
   display: 'flex',
   flexDirection: 'column',
-  padding: props.theme.spacing(8),
+  padding: props.theme.spacing(10),
   flex: 1,
   justifyContent: 'center',
   alignItems: 'center',
@@ -53,70 +57,96 @@ const ContentContainer = styled.div((props) => ({
 const Title = styled.h1((props) => ({
   ...props.theme.mont_tile_text,
   color: props.theme.colors.action.classic,
-  marginTop: props.theme.spacing(8),
+  fontSize: 24,
   textAlign: 'center',
 }));
 
 const Subtitle = styled.h2((props) => ({
-  ...props.theme.mont_tile_text,
-  fontSize: 14,
+  ...props.theme.bold_tile_text,
   color: props.theme.colors.white[0],
   marginTop: props.theme.spacing(8),
   textAlign: 'center',
 }));
 
-const ContinueButton = styled.button((props) => ({
-  ...props.theme.body_bold_m,
-  color: props.theme.colors.white[0],
-  backgroundColor: props.theme.colors.action.classic,
-  borderRadius: props.theme.radius(1),
-  marginLeft: props.theme.spacing(8),
-  marginRight: props.theme.spacing(8),
-  marginBottom: props.theme.spacing(30),
-  height: 44,
-  textAlign: 'center',
-  ':hover': {
-    background: props.theme.colors.action.classicLight,
-  },
-  ':focus': {
-    background: props.theme.colors.action.classicLight,
-    opacity: 0.6,
-  },
+const ButtonContainer = styled.div((props) => ({
+  marginLeft: props.theme.spacing(10),
+  marginRight: props.theme.spacing(10),
+  marginBottom: props.theme.spacing(20),
 }));
 
 function CreateWalletSuccess(): JSX.Element {
   const { t } = useTranslation('translation', { keyPrefix: 'WALLET_SUCCESS_SCREEN' });
   const { action } = useParams();
+  const [activeIndex, setActiveIndex] = useState(0);
 
-  const handleCloseTab = () => {
-    window.close();
+  const steps = [
+    {
+      img: Completed,
+      title: t('SCREEN_TITLE'),
+      subtitle: t('SCREEN_SUBTITLE'),
+    },
+    {
+      img: Followup,
+      title: t('SELF_CUSTODY'),
+      subtitle: t('SELF_CUSTODY_DESC'),
+    },
+    {
+      img: Followup,
+      title: t('PRIVATE'),
+      subtitle: t('PRIVATE_DESC'),
+    },
+    {
+      img: Followup,
+      title: t('ANONYMOUS'),
+      subtitle: t('ANONYMOUS_DESC'),
+    },
+    {
+      img: Followup,
+      title: t('DECENTRALIZED'),
+      subtitle: t('DECENTRALIZED_DESC'),
+    },
+  ];
+  const handleOpenWallet = () => {
+    if (action === 'restore') {
+      window.close();
+    }
+    if (activeIndex < steps.length - 1) {
+      setActiveIndex((cur) => cur + 1);
+    } else {
+      window.close();
+    }
   };
 
   return (
     <>
       <ContentContainer>
-        <img src={Completed} alt="success" />
-        <Title>{action === 'restore' ? t('RESTORE_SCREEN_TITLE') : t('SCREEN_TITLE')}</Title>
+        <img src={action === 'restore' ? Completed : steps[activeIndex].img} alt="success" />
+        <Title>{action === 'restore' ? t('RESTORE_SCREEN_TITLE') : steps[activeIndex].title}</Title>
         <Subtitle>
-          {action === 'restore' ? t('RESTORE_SCREEN_SUBTITLE') : t('SCREEN_SUBTITLE')}
+          {action === 'restore' ? t('RESTORE_SCREEN_SUBTITLE') : steps[activeIndex].subtitle}
         </Subtitle>
+        {action === 'restore' ? null : <Steps data={steps} activeIndex={activeIndex} />}
       </ContentContainer>
-      <ContinueButton onClick={handleCloseTab}>{t('CLOSE_TAB')}</ContinueButton>
-      <InstructionsContainer>
-        <RowContainer>
-          <InstructionsText>{`1. ${t('CLICK')}`}</InstructionsText>
-          <Image src={Extension} />
-        </RowContainer>
-        <RowContainer>
-          <InstructionsText>{`2. ${t('SEARCH_XVERSE')}`}</InstructionsText>
-          <Image src={Pin} />
-        </RowContainer>
-        <RowContainer>
-          <InstructionsText>{`3. ${t('CLICK')}`}</InstructionsText>
-          <Image src={Logo} />
-          <InstructionsText>{t('OPEN_WALLET')}</InstructionsText>
-        </RowContainer>
-      </InstructionsContainer>
+      <ButtonContainer>
+        <ActionButton onPress={handleOpenWallet} text={t('CLOSE_TAB')} />
+      </ButtonContainer>
+      {action === 'restore' || activeIndex === steps.length - 1 ? (
+        <InstructionsContainer>
+          <RowContainer>
+            <InstructionsText>{`1. ${t('CLICK')}`}</InstructionsText>
+            <Image src={Extension} />
+          </RowContainer>
+          <RowContainer>
+            <InstructionsText>{`2. ${t('SEARCH_XVERSE')}`}</InstructionsText>
+            <Image src={Pin} />
+          </RowContainer>
+          <RowContainer>
+            <InstructionsText>{`3. ${t('CLICK')}`}</InstructionsText>
+            <Image src={Logo} />
+            <InstructionsText>{t('OPEN_WALLET')}</InstructionsText>
+          </RowContainer>
+        </InstructionsContainer>
+      ) : null}
     </>
   );
 }
