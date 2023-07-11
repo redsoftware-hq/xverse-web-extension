8d02a027a53e70a18e8934a37de304531c0e6eb4
Ashish Baravaliya <vier.institute35@gmail.com>
2023-07-11 09:59:36 -0400

    fix: tabbar fixes

--- src/app/components/tabBar/index.tsx
+++ src/app/components/tabBar/index.tsx
@@ -5,9 +5,11 @@ import { useNavigate } from 'react-router-dom';
 import NftTab from '@assets/img/bottomTabBar/Selected_nft.svg';
 import SettingsTab from '@assets/img/bottomTabBar/setting_tab.svg';
 import StackingTab from '@assets/img/bottomTabBar/Selected_stake.svg';
+import MarketTab from '@assets/img/bottomTabBar/Selected_market.svg';
 import WalletTab from '@assets/img/bottomTabBar/Selected_wallet.svg';
 import UnselectedNftTab from '@assets/img/bottomTabBar/NFT.svg';
 import UnselectedSettingsTab from '@assets/img/bottomTabBar/unselected_setting_tab.svg';
+import UnselectedMarketTab from '@assets/img/bottomTabBar/unselected_market_tab.svg';
 import UnselectedStackingTab from '@assets/img/bottomTabBar/Stake.svg';
 import UnselectedWalletTab from '@assets/img/bottomTabBar/Wallet.svg';
 
@@ -16,11 +18,11 @@ const Container = styled.div((props) => ({
   flexDirection: 'row',
   alignItems: 'center',
   width: '100%',
-  height: 64,
+  height: 76,
   background: props.theme.colors.background.darkbg,
   justifyContent: 'space-between',
   paddingLeft: props.theme.spacing(10),
-  paddingBottom: props.theme.spacing(15),
+  paddingBottom: props.theme.spacing(10),
   paddingRight: props.theme.spacing(10),
 }));
 const RowContainer = styled.div((props) => ({
@@ -28,15 +30,15 @@ const RowContainer = styled.div((props) => ({
   flexDirection: 'row',
   alignItems: 'center',
   width: '100%',
-  height: 64,
+  height: 56,
   background: props.theme.colors.background.navigation,
   justifyContent: 'space-between',
   paddingLeft: props.theme.spacing(20),
-  borderRadius: props.theme.radius(2),
+  borderRadius: props.theme.radius(1),
   paddingRight: props.theme.spacing(20),
   border: `1px solid #A8B9F433`,
   ":hover": {
-    background: 'linear-gradient(90deg, #A8B9F4 20%, #FFFFFF 90%);'
+    background: 'rgba(255, 255, 255, 0.9)'
   }
 }));
 
@@ -45,7 +47,7 @@ const Button = styled.button({
   zIndex: 2,
 });
 
-export type Tab = 'dashboard' | 'nft' | 'stacking' | 'settings';
+type Tab = 'dashboard' | 'nft' | 'market' | 'stacking';
 
 interface Props {
   tab: Tab;
@@ -77,20 +79,16 @@ function BottomTabBar({ tab }: Props) {
   };
 
   const handleNftButtonClick = () => {
-    if (tab !== 'nft') {
-      navigate('/nft-dashboard');
-    }
+    if (tab !== 'nft') { navigate('/nft-dashboard'); }
+  };
+
+  const handleMarketButtonClick = () => {
+    if (tab !== 'market') { navigate('/market'); }
   };
 
   const handleStackingButtonClick = () => {
     if (tab !== 'stacking') {
       navigate('/stacking');
-    }
-  };
-
-  const handleSettingButtonClick = () => {
-    if (tab !== 'settings') {
-      navigate('/settings');
     }
   };
 
@@ -104,17 +102,14 @@ function BottomTabBar({ tab }: Props) {
           size="24"
         />
       </Button>
-      <Button onClick={handleStackingButtonClick}>
-        <img src={tab === 'stacking' ? StackingTab : UnselectedStackingTab} alt="stacking" />
+      <Button onClick={handleMarketButtonClick}>
+        <img src={tab === 'market' ? MarketTab : UnselectedMarketTab} alt="market" />
       </Button>
       <Button onClick={handleNftButtonClick}>
         <img src={tab === 'nft' ? NftTab : UnselectedNftTab} alt="nft" />
       </Button>
-      <Button onClick={handleSettingButtonClick}>
-        <Gear
-          color={tab === 'settings' ? theme.colors.white_0 : theme.colors.white_600}
-          size="24"
-        />
+      <Button onClick={handleStackingButtonClick}>
+        <img src={tab === 'stacking' ? StackingTab : UnselectedStackingTab} alt="stacking" />
       </Button>
     </RowContainer>
     </Container>
