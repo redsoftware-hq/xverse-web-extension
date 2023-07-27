3257c34b542d5d3cf1407e1ffdc65a00caf543c0
Ashish Baravaliya <vier.institute35@gmail.com>
2023-07-27 10:06:34 -0400

    fix: copy button

--- src/app/components/seedPhraseView/word.tsx
+++ src/app/components/seedPhraseView/word.tsx
@@ -2,14 +2,14 @@ import styled from 'styled-components';
 
 const SeedWord = styled.p((props) => ({
   ...props.theme.body_medium_m,
-  marginTop: props.theme.spacing(16),
+  marginTop: props.theme.spacing(11),
 }));
 
 const OrdinalNumber = styled.p((props) => ({
   ...props.theme.body_medium_m,
   marginLeft: props.theme.spacing(7),
   marginRight: props.theme.spacing(1.5),
-  marginTop: props.theme.spacing(16),
+  marginTop: props.theme.spacing(11),
   color: props.theme.colors.white['0'],
 }));
 
