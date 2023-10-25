ad1ef815e2e09baddc1aa225e9a2e30cb2042e4f
sanketdebnath24 <sanket.debnath@redsoftware.in>
2023-10-25 16:20:10 +0530

    rm: remove buy button for not BTC coins

--- src/app/screens/coinDashboard/coinHeader.tsx
+++ src/app/screens/coinDashboard/coinHeader.tsx
@@ -437,14 +437,16 @@ export default function CoinHeader(props: CoinBalanceProps) {
                     onPress={() => navigate(`/receive/${coin}`)}
                   />
                 </ButtonContainer>
-                <ButtonContainer>
-                  <SmallActionButton
-                    isOpaque
-                    isRound
-                    src={Buy}
-                    onPress={() => navigate(`/buy/${coin}`)}
-                  />
-                </ButtonContainer>
+                {coin === 'BTC' && (
+                  <ButtonContainer>
+                    <SmallActionButton
+                      isOpaque
+                      isRound
+                      src={Buy}
+                      onPress={() => navigate(`/buy/${coin}`)}
+                    />
+                  </ButtonContainer>
+                )}
               </>
             ) : (
               <>
@@ -456,14 +458,16 @@ export default function CoinHeader(props: CoinBalanceProps) {
                     onPress={() => navigate(coin === 'brc20' ? '/receive/ORD' : `/receive/${coin}`)}
                   />
                 </ButtonContainer>
-                <ButtonContainer>
-                  <SmallActionButton
-                    isOpaque
-                    isRound
-                    src={Buy}
-                    onPress={() => navigate(`/buy/${stepsData[currentActiveIndex]}`)}
-                  />
-                </ButtonContainer>
+                {coin === 'BTC' && (
+                  <ButtonContainer>
+                    <SmallActionButton
+                      isOpaque
+                      isRound
+                      src={Buy}
+                      onPress={() => navigate(`/buy/${stepsData[currentActiveIndex]}`)}
+                    />
+                  </ButtonContainer>
+                )}
               </>
             )}
 
