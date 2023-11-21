2f221f89f4d62e53fa85af7d422af3a3b57895ef
sanketdebnath24 <sanket.debnath@redsoftware.in>
2023-11-21 17:46:04 +0530

    add: ui for connecting

--- src/app/screens/btcSelectAddressScreen/index.tsx
+++ src/app/screens/btcSelectAddressScreen/index.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import XverseLogo from '@assets/img/settings/logo.svg';
+import Logo from '@assets/img/pill.png';
 import DropDownIcon from '@assets/img/transactions/dropDownIcon.svg';
 import { useTranslation } from 'react-i18next';
 import { useEffect, useState } from 'react';
@@ -21,11 +21,9 @@ import AccountView from './accountView';
 const TitleContainer = styled.div({
   display: 'flex',
   flexDirection: 'column',
-  alignItems: 'center',
+  alignItems: 'left',
   justifyContent: 'center',
-  overflow: 'hidden',
-  marginLeft: 30,
-  marginRight: 30,
+  // overflow: 'hidden',
 });
 
 const DropDownContainer = styled.div({
@@ -38,11 +36,25 @@ const DropDownContainer = styled.div({
 });
 
 const LogoContainer = styled.div((props) => ({
-  padding: props.theme.spacing(11),
-  marginBottom: props.theme.spacing(16),
-  borderBottom: `1px solid ${props.theme.colors.background.elevation3}`,
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  padding: props.theme.spacing(7),
 }));
 
+const Caution = styled.div({
+  borderRadius: '15px',
+  border: '1px solid #F00',
+  background: 'rgba(255, 0, 0, 0.20)',
+  color: '#F00',
+  padding: '6px 14px 6px 14px',
+  textAlign: 'center',
+  fontFamily: 'MontRegular',
+  fontSize: '14px',
+  fontWeight: 600,
+  lineHeight: 'normal',
+});
 const AddressContainer = styled.div((props) => ({
   background: props.theme.colors.background.elevation2,
   borderRadius: 40,
@@ -63,7 +75,8 @@ const AccountListContainer = styled(animated.div)((props) => ({
   height: 214,
   marginTop: props.theme.spacing(9.5),
   boxShadow: '0px 8px 104px rgba(0, 0, 0, 0.5)',
-  background: props.theme.colors.background.elevation2,
+  background:
+    'radial-gradient(489.09% 91.61% at 89.79% 22.85%, rgba(56, 60, 78, 0.20) 0%, rgba(13, 14, 18, 0.20) 100%)',
   '&::-webkit-scrollbar': {
     display: 'none',
   },
@@ -79,43 +92,49 @@ const TopImage = styled.img({
 
 const FunctionTitle = styled.h1((props) => ({
   ...props.theme.body_bold_l,
-  color: props.theme.colors.white['0'],
-  marginTop: 16,
+  color: '#D23403',
+  fontFamily: 'MontBold',
+  fontSize: '32px',
+  fontStyle: 'normal',
+  fontWeight: 800,
+  lineHeight: 'normal',
 }));
 
 const AccountContainer = styled.button((props) => ({
-  background: props.theme.colors.background.elevation1,
-  border: `1px solid ${props.theme.colors.background.elevation3}`,
+  background:
+    'radial-gradient(489.09% 91.61% at 89.79% 22.85%, rgba(56, 60, 78, 0.20) 0%, rgba(13, 14, 18, 0.20) 100%)',
+  border: '1px solid rgba(168, 185, 244, 0.15)',
   borderRadius: 8,
   width: '100%',
-  padding: '12px 16px',
+  padding: '16px 20px 16px 20px;',
   display: 'flex',
   flexDirection: 'row',
   marginTop: props.theme.spacing(4),
-  ':hover': {
-    background: props.theme.colors.background.elevation2,
-  },
 }));
 
 const AccountText = styled.h1((props) => ({
   ...props.theme.body_medium_m,
-  color: props.theme.colors.white['400'],
-  marginTop: 24,
+  fontFamily: 'MontRegular',
+  fontSize: 18,
+  color: props.theme.colors.white[0],
+  marginLeft: 8,
+  marginTop: 16,
+  marginBottom: 16,
 }));
 
-const DappTitle = styled.h2((props) => ({
-  ...props.theme.body_m,
-  color: props.theme.colors.white['200'],
-  marginTop: 12,
-  textAlign: 'center',
-}));
+// const DappTitle = styled.h2((props) => ({
+//   ...props.theme.body_m,
+//   color: props.theme.colors.white['200'],
+//   marginTop: 12,
+//   textAlign: 'center',
+// }));
 
-const AddressTextTitle = styled.h1((props) => ({
-  ...props.theme.body_medium_l,
-  color: props.theme.colors.white['0'],
-  fontSize: 10,
-  textAlign: 'center',
-}));
+// const AddressTextTitle = styled.h1((props) => ({
+//   ...props.theme.body_medium_l,
+//   color: props.theme.colors.white['0'],
+//   fontSize: 10,
+//   textAlign: 'center',
+// }));
 
 const OuterContainer = styled(animated.div)({
   display: 'flex',
@@ -133,13 +152,13 @@ const ButtonsContainer = styled.div((props) => ({
   marginTop: 82,
 }));
 
-const BitcoinDot = styled.div((props) => ({
-  borderRadius: 20,
-  background: props.theme.colors.feedback.caution,
-  width: 6,
-  marginRight: props.theme.spacing(3),
-  height: 6,
-}));
+// const BitcoinDot = styled.div((props) => ({
+//   borderRadius: 20,
+//   background: props.theme.colors.feedback.caution,
+//   width: 6,
+//   marginRight: props.theme.spacing(3),
+//   height: 6,
+// }));
 
 const AccountListRow = styled.div((props) => ({
   paddingLeft: 16,
@@ -155,23 +174,34 @@ const TransparentButtonContainer = styled.div((props) => ({
   width: '100%',
 }));
 
-const OrdinalImage = styled.img({
-  width: 12,
-  height: 12,
-  marginRight: 8,
+const Pill = styled.img({
+  width: 60,
+  height: 34,
 });
+// const OrdinalImage = styled.img({
+//   width: 12,
+//   height: 12,
+//   marginRight: 8,
+// });
 
+const Paragraph = styled.p((props) => ({
+  ...props.theme.body_l,
+  fontFamily:'MontRegular',
+  color: props.theme.colors.white['200'],
+  textAlign: 'left',
+  fontSize:18,
+  marginTop: props.theme.spacing(8),
+  marginBottom: props.theme.spacing(8),
+  paddingLeft: props.theme.spacing(3),
+  paddingRight: props.theme.spacing(3),
+}));
 function BtcSelectAddressScreen() {
   const [loading, setLoading] = useState(false);
   const [showAccountList, setShowAccountList] = useState(false);
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { t } = useTranslation('translation', { keyPrefix: 'SELECT_BTC_ADDRESS_SCREEN' });
-  const {
-    selectedAccount,
-    accountsList,
-    network,
-  } = useWalletSelector();
+  const { selectedAccount, accountsList, network } = useWalletSelector();
   const { payload, approveBtcAddressRequest, cancelAddressRequest } = useBtcAddressRequest();
   const springProps = useSpring({
     transform: showAccountList ? 'translateY(0%)' : 'translateY(100%)',
@@ -242,18 +272,24 @@ function BtcSelectAddressScreen() {
 
   useEffect(() => {
     switchAccountBasedOnRequest();
+    window.resizeTo(374.6,600);
   }, []);
 
+  function getName() {
+    return selectedAccount!?.bnsName ?? `Account ${`${(selectedAccount!?.id ?? 0) + 1}`}`;
+  }
   return (
     <>
       <LogoContainer>
-        <img src={XverseLogo} alt="xverse logo" />
+        <Pill src={Logo} alt="orange-pill-logo" />
+        <Caution>Caution</Caution>
       </LogoContainer>
       <OuterContainer style={styles}>
         <TitleContainer>
-          <TopImage src={DappPlaceholderIcon} alt="Dapp Logo" />
+          {/* <TopImage src={DappPlaceholderIcon} alt="Dapp Logo" /> */}
           <FunctionTitle>{t('TITLE')}</FunctionTitle>
-          <div style={{ display: 'flex', alignItems: 'center' }}>
+          <Paragraph>This site is requesting to connect to your Orange Pill wallet. Please ensure that this site is authorized to access your wallet before proceeding.</Paragraph>
+          {/* <div style={{ display: 'flex', alignItems: 'center' }}>
             {payload.purposes.map((purpose) => (purpose === AddressPurposes.PAYMENT ? (
               <AddressContainer>
                 <BitcoinDot />
@@ -265,14 +301,15 @@ function BtcSelectAddressScreen() {
                 <AddressTextTitle>{t('ORDINAL_ADDRESS')}</AddressTextTitle>
               </AddressContainer>
             )))}
-          </div>
-          <DappTitle>{payload.message}</DappTitle>
+          </div> */}
+          {/* <DappTitle>{payload.message}</DappTitle> */}
         </TitleContainer>
         {showAccountList ? (
           <AccountListContainer style={springProps}>
             {accountsList.map((account) => (
               <AccountListRow>
                 <AccountRow
+                  usedInPopup
                   key={account.stxAddress}
                   account={account}
                   isSelected={isAccountSelected(account)}
@@ -285,7 +322,7 @@ function BtcSelectAddressScreen() {
           </AccountListContainer>
         ) : (
           <>
-            <AccountText>{t('ACCOUNT')}</AccountText>
+            <AccountText>{getName()}</AccountText>
             <AccountContainer onClick={onChangeAccount}>
               <AccountView account={selectedAccount!} isBitcoinTx />
               <DropDownContainer>
